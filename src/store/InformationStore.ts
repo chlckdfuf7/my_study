6f89108a571a2a089353fb9bf282603d2eac41ae
changyeol_choi <changyeol_choi@tmax.co.kr>
2025-01-15 21:56:16 +0900

    [feat] 필요 패키지 추가 boundMethod 사용을 위한 auto-bind 패키지 추가 및 그에 따른 설정파일 수정

--- src/store/InformationStore.ts
+++ src/store/InformationStore.ts
@@ -1,4 +1,5 @@
 import { makeObservable, observable } from "mobx";
+import { boundMethod } from 'autobind-decorator';
 
 // 프로젝트에서 전역으로 관리할 변수들을 저장하는 store
 // 각 페이지 별 필요한 정보들을 각각의 context로 분리해서 관리할 필요가 있을지 추후 점검 후 필요시 변경하자
@@ -15,18 +16,22 @@ class InformationStore {
         this.page = 0;
     }
 
+    @boundMethod
     public getPage(): number {
         return this.page;
     }
 
+    @boundMethod
     public setPage(idx: number) {
         this.page = idx;
     }
 
+    @boundMethod
     public getCity(): string | undefined {
         return this.city;
     }
 
+    @boundMethod
     public setCity(name: string) {
         this.city = name;
     }
