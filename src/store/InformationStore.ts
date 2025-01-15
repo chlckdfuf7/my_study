a1e20327b6c4d801e207f38c4b27bd6991b53732
changyeol_choi <changyeol_choi@tmax.co.kr>
2025-01-15 18:01:44 +0900

    [feat]observing할 대상 설정 store내에 observing할 대상을 설정

--- src/store/InformationStore.ts
+++ src/store/InformationStore.ts
@@ -1,10 +1,12 @@
-import { makeObservable } from "mobx";
+import { makeObservable, observable } from "mobx";
 
 // 프로젝트에서 전역으로 관리할 변수들을 저장하는 store
 // 각 페이지 별 필요한 정보들을 각각의 context로 분리해서 관리할 필요가 있을지 추후 점검 후 필요시 변경하자
 class InformationStore {
+    @observable
     private page: number;
     
+    @observable
     private city: string | undefined;
     
     public constructor() {
